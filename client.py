1edd4f345264197483b62d9fa671678285682282
Eric Joseph Anderson <eja1@cs1-10-71.ucc.ie>
2020-02-18 11:05:12 +0000

    Ability for Client and Server to talk with each other

--- client.py
+++ client.py
@@ -9,9 +9,17 @@ sock = socket(AF_INET, SOCK_STREAM)
 
 # Acquires the host name and IP address from the computer using this program
 #host_name = gethostname()
-host_name = 'XFA-27'
-IP_address = gethostbyname(host_name)
+print('Welcome to the simple client and server chat program')
+def connect():
+    try:
+        host_name = input('Please enter the name of the host that you want to connect to :: ')
+        IP_address = gethostbyname(str(host_name))
+        return host_name, IP_address
+    except:
+        print('ERROR: Failed to connect')
+        connect()
 
+host_name, IP_address = connect()
 # Prints the Host name and IP address to the console
 print('Host name is :: ' + host_name)
 print('IP address :: ' + IP_address)
@@ -25,23 +33,24 @@ print('connecting to server at %s port %s' % server_address)
 sock.connect(server_address)
 
 try:
+    while True:
+        # Ask for user to input a message into the console, and then send that message.
+        message = input('Enter a message to send to the server :: ')
+        print('sending "%s"' % message)
+        print('Awaiting response from the server')
+        # Data is transmitted to the server with sendall()
+        # encode() function returns bytes object
+        sock.sendall(message.encode())
 
-    # Ask for user to input a message into the console, and then send that message. 
-    message = input('Enter a message to send to the server :: ')
-    print('sending "%s"' % message)
-    # Data is transmitted to the server with sendall()
-    # encode() function returns bytes object
-    sock.sendall(message.encode())
-
-    # Look for the response
-    amount_received = 0
-    amount_expected = len(message)
-    while amount_received < amount_expected:
+        # Look for the response
+        amount_received = 0
+        # amount_expected = len(message)
+        # while amount_received < amount_expected:
     	# Data is read from the connection with recv()
         # decode() function returns string object
-        data = sock.recv(16).decode()
+        data = sock.recv(2000).decode()
         amount_received += len(data)
-        # Get the current date and time 
+        # Get the current date and time
         date = datetime.today().strftime("%B %d, %Y")
         time =  datetime.now().strftime("%H:%M:%S")
         # print data received from the server, and the time and date the client got the data
