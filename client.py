a6a5e03ed95ae26e47a76493f129c242b31f5191
devEricA <eric.anderson413@gmail.com>
2020-02-18 09:13:25 -0600

    Fixed Exception Handling & Additional Documentation

--- client.py
+++ client.py
@@ -7,45 +7,51 @@ from datetime import *
 #(SOCK_STREAM is used for TCP)
 sock = socket(AF_INET, SOCK_STREAM)
 
-# Acquires the host name and IP address from the computer using this program
-#host_name = gethostname()
+# Greeting for user using the client
 print('Welcome to the simple client and server chat program')
+
+# Python function used in order to ensure a connection to the server
+# without a crash
 def connect():
     try:
+        # Acquires the host name and IP address from user input using input
         host_name = input('Please enter the name of the host that you want to connect to :: ')
         IP_address = gethostbyname(str(host_name))
-        return host_name, IP_address
+        # set values for host using the host name - meaning this machine and port number 10000
+        # the machine address and port number have to be the same as the server is using.
+        server_address = (host_name, 10000)
+        # output to terminal some info on the address details
+        print('connecting to server at %s port %s' % server_address)
+        # Connect the socket to the host and port
+        sock.connect(server_address)
+        # Return the host name and address so they can be used later
+        return str(host_name), IP_address
     except:
-        print('ERROR: Failed to connect')
-        connect()
+        # In the event where the user fails to make a connection
+        # print an error message
+        print('ERROR: Failed to connect.')
+        # Call the connect method so the user can connect again
+        return connect()
 
+# Get the host name and IP address using the previously created method.
 host_name, IP_address = connect()
+# host_name = connect()
 # Prints the Host name and IP address to the console
 print('Host name is :: ' + host_name)
 print('IP address :: ' + IP_address)
-
-# set values for host using the host name - meaning this machine and port number 10000
-# the machine address and port number have to be the same as the server is using.
-server_address = (host_name, 10000)
-# output to terminal some info on the address details
-print('connecting to server at %s port %s' % server_address)
-# Connect the socket to the host and port
-sock.connect(server_address)
-
 try:
+    #Need a forever while loop in order to allow the client to talk to the server
     while True:
         # Ask for user to input a message into the console, and then send that message.
         message = input('Enter a message to send to the server :: ')
         print('sending "%s"' % message)
+        # Once message is sent to the server, the client awaits a response
         print('Awaiting response from the server')
         # Data is transmitted to the server with sendall()
         # encode() function returns bytes object
         sock.sendall(message.encode())
-
         # Look for the response
         amount_received = 0
-        # amount_expected = len(message)
-        # while amount_received < amount_expected:
     	# Data is read from the connection with recv()
         # decode() function returns string object
         data = sock.recv(2000).decode()
@@ -57,5 +63,6 @@ try:
         print('received "%s" on %s at %s' % (data, date, time))
 
 finally:
+    # Close the socket
     print('closing socket')
     sock.close()
